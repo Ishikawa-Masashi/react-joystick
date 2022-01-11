07281a69286d04d12993dae93ef225936d99c87f
IshikawaMasashi <IshikawaMasashi@outlook.com>
2022-01-11 21:34:47 +0900

    first commit

---
+++ example/Album.tsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+import { Joystick } from '../src/Joystick';
+import { ParseError } from 'katex';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://ishikawa-masashi.github.io/">
+        Ishikawa Masashi
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const _ = String.raw;
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function renderError(error: ParseError | TypeError) {
+  return (
+    <p>
+      <b>Custom</b> error message: {error.name}
+    </p>
+  );
+}
+
+const theme = createTheme();
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function Album() {
+  const [value, setValue] = React.useState('y = k * x + c');
+  const handleChange = React.useCallback(
+    (event) => {
+      setValue(event.target.value);
+    },
+    [setValue]
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            react-katex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Stack
+              direction="row"
+              spacing={0}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <div className="react-icon" />
+              <Typography
+                component="h1"
+                variant="h2"
+                // align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                React Jyostick
+              </Typography>
+            </Stack>
+            <Typography
+              variant="h4"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Demo of react-katex
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Dynamic formula
+            </Typography>
+
+            <Joystick
+              start={(evt) => console.log('Started')}
+              move={(evt) => console.log(evt)}
+              stop={(evt) => console.log('Stopped')}
+            />
+            <small>
+              You can notice that in the code is written the custom macro (via{' '}
+              <code>props.settings</code>) for interpunct so you can use &quot;
+              <code>*</code>
+              &quot; instead of &quot;
+              <code>\cdot</code>
+              &quot;.
+            </small>
+            <small>
+              You can notice that in the code is written the custom macro (via{' '}
+              <code>props.settings</code>) for interpunct so you can use &quot;
+              <code>*</code>
+              &quot; instead of &quot;
+              <code>\cdot</code>
+              &quot;.
+            </small>
+            <h2>Inline math</h2>
+            <h2>Block math</h2>
+            <span>
+              via <code>props.children</code>:
+            </span>
+            <span>
+              via <code>props.math</code>:
+            </span>
+            <h2>Error handling</h2>
+            <span>
+              <code>props.errorColor</code>:
+            </span>
+            <span>
+              <code>props.renderError</code>:
+            </span>
+            <span>simple expression:</span>
+            <h2>Custom wrapper component</h2>
+          </Container>
+        </Box>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          {/* Copyright © 2021 Ishikawa Masashi. */}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          {/* Something here to give the footer a purpose! */}
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
---
+++ src/Joystick.tsx
@@ -0,0 +1,269 @@
+import React, { useEffect } from 'react';
+
+export interface IJoystickProps {
+  size?: number;
+  baseColor?: string;
+  stickColor?: string;
+  throttle?: number;
+  disabled?: boolean;
+  move?: (event: IJoystickUpdateEvent) => void;
+  stop?: (event: IJoystickUpdateEvent) => void;
+  start?: (event: IJoystickUpdateEvent) => void;
+}
+enum InteractionEvents {
+  MouseDown = 'mousedown',
+  MouseMove = 'mousemove',
+  MouseUp = 'mouseup',
+  TouchStart = 'touchstart',
+  TouchMove = 'touchmove',
+  TouchEnd = 'touchend',
+}
+
+export interface IJoystickUpdateEvent {
+  type: 'move' | 'stop' | 'start';
+  x: number | null;
+  y: number | null;
+  direction: JoystickDirection | null;
+}
+
+export interface IJoystickState {
+  dragging: boolean;
+  coordinates?: IJoystickCoordinates;
+}
+type JoystickDirection = 'FORWARD' | 'RIGHT' | 'LEFT' | 'BACKWARD';
+export interface IJoystickCoordinates {
+  relativeX: number;
+  relativeY: number;
+  axisX: number;
+  axisY: number;
+  direction: JoystickDirection;
+}
+
+export const Joystick: React.VFC<IJoystickProps> = (props) => {
+  const { baseColor = '#000033', stickColor = '#3D59AB' } = props;
+  const _stickRef = React.useRef<HTMLDivElement>(null);
+  const _baseRef = React.useRef<HTMLDivElement>(null);
+  //   const _throttleMoveCallback: (data: any) => void;
+  //   const _boundMouseUp: EventListenerOrEventListenerObject;
+  //   const _baseSize: number;
+  //   const _parentRect: ClientRect;
+  const _parentRectRef = React.useRef<DOMRect>();
+  //   const _boundMouseMove: (event: any) => void;
+
+  //   constructor(props: IJoystickProps) {
+  //     super(props);
+  // this.state = {
+  //   dragging: false,
+  // };
+  //   const [dragging, setDragging] = React.useState(false);
+  const draggingRef = React.useRef(false);
+  //   const [coordinates, setCoordinates] = React.useState<IJoystickCoordinates>();
+  const coordinatesRef = React.useRef<IJoystickCoordinates | undefined>(
+    undefined
+  );
+
+  // this._stickRef = React.createRef();
+  // this._baseRef = React.createRef();
+
+  const _throttleMoveCallback = (() => {
+    let lastCall = 0;
+    return (event: IJoystickUpdateEvent) => {
+      const now = new Date().getTime();
+      const throttleAmount = props.throttle || 0;
+      if (now - lastCall < throttleAmount) {
+        return;
+      }
+      lastCall = now;
+      if (props.move) {
+        return props.move(event);
+      }
+    };
+  })();
+
+  const _boundMouseUp = () => {
+    _mouseUp();
+  };
+  const _boundMouseMove = (event: MouseEvent | TouchEvent) => {
+    _mouseMove(event);
+  };
+  //   }
+
+  const _updatePos = (coordinates: IJoystickCoordinates) => {
+    window.requestAnimationFrame(() => {
+      //   this.setState({
+      //     coordinates,
+      //   });
+      //   setCoordinates(coordinates);
+      coordinatesRef.current = coordinates;
+
+      _stickRef.current!.style.transform = `translate3d(${coordinatesRef.current.relativeX}px, ${coordinatesRef.current.relativeY}px, 0)`;
+    });
+    _throttleMoveCallback({
+      type: 'move',
+      x: coordinates.relativeX,
+      y: -coordinates.relativeY,
+      direction: coordinates.direction,
+    });
+  };
+
+  const _mouseDown = (e: any) => {
+    if (props.disabled !== true) {
+      _parentRectRef.current = _baseRef.current!.getBoundingClientRect();
+
+      //   this.setState({
+      //     dragging: true,
+      //   });
+      draggingRef.current = true;
+
+      if (e.type === InteractionEvents.MouseDown) {
+        window.addEventListener(InteractionEvents.MouseUp, _boundMouseUp);
+        window.addEventListener(InteractionEvents.MouseMove, _boundMouseMove);
+      } else {
+        window.addEventListener(InteractionEvents.TouchEnd, _boundMouseUp);
+        window.addEventListener(InteractionEvents.TouchMove, _boundMouseMove);
+      }
+
+      if (props.start) {
+        props.start({
+          type: 'start',
+          x: null,
+          y: null,
+          direction: null,
+        });
+      }
+    }
+  };
+
+  const _getDirection = (atan2: number): JoystickDirection => {
+    if (atan2 > 2.35619449 || atan2 < -2.35619449) {
+      return 'FORWARD';
+    } else if (atan2 < 2.35619449 && atan2 > 0.785398163) {
+      return 'RIGHT';
+    } else if (atan2 < -0.785398163) {
+      return 'LEFT';
+    }
+    return 'BACKWARD';
+  };
+  const _getWithinBounds = (value: number): number => {
+    const halfBaseSize = _baseSize / 2;
+    if (value > halfBaseSize) {
+      return halfBaseSize;
+    }
+    if (value < -halfBaseSize) {
+      return halfBaseSize * -1;
+    }
+    return value;
+  };
+  const _mouseMove = (event: MouseEvent | TouchEvent) => {
+    if (draggingRef.current) {
+      event.stopPropagation();
+      event.preventDefault();
+      let absoluteX = null;
+      let absoluteY = null;
+      if (event.type === InteractionEvents.MouseMove) {
+        absoluteX = (event as MouseEvent).clientX;
+        absoluteY = (event as MouseEvent).clientY;
+      } else {
+        absoluteX = (event as TouchEvent).touches[0].clientX;
+        absoluteY = (event as TouchEvent).touches[0].clientY;
+      }
+
+      const relativeX = _getWithinBounds(
+        absoluteX - _parentRectRef.current!.left - _baseSize / 2
+      );
+      const relativeY = _getWithinBounds(
+        absoluteY - _parentRectRef.current!.top - _baseSize / 2
+      );
+      const atan2 = Math.atan2(relativeX, relativeY);
+
+      _updatePos({
+        relativeX,
+        relativeY,
+        direction: _getDirection(atan2),
+        axisX: absoluteX - _parentRectRef.current!.left,
+        axisY: absoluteY - _parentRectRef.current!.top,
+      });
+    }
+  };
+
+  const _mouseUp = () => {
+    // this.setState({
+    //   dragging: false,
+    //   coordinates: undefined,
+    // });
+    // setDragging(false);
+    draggingRef.current = false;
+    // setCoordinates(undefined);
+    coordinatesRef.current = undefined;
+
+    _stickRef.current!.style.transform = 'unset';
+
+    window.removeEventListener('mouseup', _boundMouseUp);
+    window.removeEventListener('mousemove', _boundMouseMove);
+
+    if (props.stop) {
+      props.stop({
+        type: 'stop',
+        x: null,
+        y: null,
+        direction: null,
+      });
+    }
+  };
+  const _getBaseStyle = () => {
+    return {
+      height: `${_baseSize}px`,
+      width: `${_baseSize}px`,
+      background: baseColor,
+      borderRadius: _baseSize,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  };
+  const _getStickStyle = () => {
+    const stickSize = `${_baseSize / 1.5}px`;
+
+    let stickStyle = {
+      background: stickColor,
+      cursor: 'move',
+      height: stickSize,
+      width: stickSize,
+      borderRadius: _baseSize,
+      flexShrink: 0,
+    };
+
+    if (draggingRef.current && coordinatesRef.current !== undefined) {
+      stickStyle = Object.assign({}, stickStyle, {
+        position: 'absolute',
+        transform: `translate3d(${coordinatesRef.current.relativeX}px, ${coordinatesRef.current.relativeY}px, 0)`,
+      });
+    }
+    return stickStyle;
+  };
+  //   render() {
+  //   this._baseSize = this.props.size || 100;
+  const _baseSize = props.size || 100;
+  const baseStyle = _getBaseStyle();
+  const stickStyle = _getStickStyle();
+  return (
+    <div
+      className={props.disabled ? 'joystick-base-disabled' : ''}
+      onMouseDown={_mouseDown}
+      onTouchStart={_mouseDown}
+      ref={_baseRef}
+      style={baseStyle}
+    >
+      <div
+        ref={_stickRef}
+        className={props.disabled ? 'joystick-disabled' : ''}
+        style={stickStyle}
+        // tabIndex={0}
+        // onBlur={(evt) => {
+        //   _mouseUp();
+        // }}
+      ></div>
+    </div>
+  );
+  //   }
+};
